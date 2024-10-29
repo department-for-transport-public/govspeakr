d51120768cbedeca9254cd2e168ef46b63e789c1
Francesca Bryden <francesca.bryden@dft.gov.uk>
2024-10-29 09:55:39 +0000

    Improved image adding documentation

--- R/add_image.R
+++ R/add_image.R
@@ -1,16 +1,24 @@
 #' Add images which are not auto-generated during the knitting process
 #' to all publishing formats.
-#' All images must be of size 960 x 640 pixels to allow upload to the
-#' Whitehall publishing system, and must be saved with a filename which
-#' starts and ends with sequential numbering.
-#' For example, if the image will be the third graphic in the final
-#' publication, it can be saved as "03-image-1.png".
+#' Images can now be saved in any format compatible with Whitehall Publisher
+#' (png or svg), and with any meaningful file name.
 #' @param file_name string giving the file name of the image only
 #' @param folder string giving the directory the image is stored in,
 #' defaults to "graphs".
 #' @export
+#' @importFrom knitr asis_output
 #' @name add_image
-#' @title Highlight in bold key words in summary text
+#' @title Convert an image file name into a html path
+#' @returns A plain text string which embeds the specified image into any output format
+#' @examples
+#' \dontrun{
+#' ##Image in default folder (graphs)
+#' add_image("rail_usage_infographic.svg")
+#'
+#' ##Image in different folder
+#' add_image("rail_usage_infographic.svg", folder = "img")
+#' }
+#'
 add_image <- function(file_name, folder = "graphs") {
   knitr::asis_output(paste0("![](", folder, "/", file_name, ")<!-- -->"))
 }
