345d0b1833ef3b8c2ce957dd2b9337ebc91b6d23
Francesca Bryden <francesca.bryden@dft.gov.uk>
2024-12-02 11:59:51 +0000

    Update documentation

--- R/callout_box.R
+++ R/callout_box.R
@@ -1,17 +1,19 @@
-#' Adds conditional CTA tags in govspeak only. Displays as text in a box in
-#' govspeak, and as simple text only in other formats.
+#' Adds conditional CTA tags in govspeak only. Displays as text in a box on Whitehall publisher,
+#' as a equivalent style HTML box in the html output if needed,
+#' and as simple text only in other formats.
 #' @param text string to publish in a callout box
 #' @param format string which identifies the outputs you would like the
-#' callout box tags to appear in. Set to HTML as default.
+#' callout box tags to appear in. Set to HTML as default. If this is not
+#' @importFrom knitr opts_knit asis_output
 #' @export
 #' @name callout_box
 #' @title Include a callout box in govspeak output
 callout_box <- function(text, format = "html") {
-  if (knitr::opts_knit$get("rmarkdown.pandoc.to") %in% format) {
+  if (opts_knit$get("rmarkdown.pandoc.to") %in% format) {
 
       knitr::asis_output(paste("$CTA", text, "$CTA", sep = "\n"))
 
-  }else if (knitr::opts_knit$get("rmarkdown.pandoc.to") == "html") {
+  } else if (opts_knit$get("rmarkdown.pandoc.to") == "html") {
 
     #Nice formatting for HTML output if required
 
