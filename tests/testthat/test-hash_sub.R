afe4dcf730e0cfa719eb791b1ecf195a3c5a7030
Francesca Bryden <francesca.bryden@dft.gov.uk>
2024-12-03 10:43:06 +0000

    Rename hash sub

--- tests/testthat/test-hash_sub.R
+++ tests/testthat/test-hash_sub.R
@@ -1,16 +1,17 @@
-test_that("default substitution works as expected", {
+test_that("hash_sub default substitution works as expected", {
   expect_identical(hash_sub("##", sub_type = TRUE), "###")
   expect_identical(hash_sub("###", sub_type = TRUE), "####")
   expect_identical(hash_sub("## This is text", sub_type = TRUE), "### This is text")
 })
 
-test_that("false sub_type does nothing", {
+test_that("hash_sub does nothing when sub type is FALSE", {
   expect_identical(hash_sub("##", sub_type = FALSE), "##")
   expect_identical(hash_sub("###", sub_type = FALSE), "###")
   expect_identical(hash_sub("## This is text", sub_type = FALSE), "## This is text")
 })
 
-test_that("selecting which strings to sub works", {
+test_that("hash_sub allows selecting which strings to sub", {
+  expect_identical(hash_sub("## ", sub_type = c("#")), "## ")
   expect_identical(hash_sub("## ", sub_type = c("#|##")), "### ")
   expect_identical(hash_sub("### ", sub_type = c("#|##")), "### ")
   expect_identical(hash_sub("## This is text", sub_type = c("#|##")), "### This is text")
